012b3144849e54ed8804fea53673086d68aa2cdb
Anvis Li <anvis.li@lonsec.com.au>
2017-08-10 16:29:59 +1000

    create a simple form first

--- class/class.MR.php
+++ class/class.MR.php
@@ -1,1 +1,25 @@
- 
+<?php
+class MR{
+
+	private $upperX = 0;
+	private $upperY = 0;
+	private $currentX = 0;
+	private $currentY = 0;
+	private $direction = null;
+
+	public function moveAction() {
+	}
+	public function spinAction($d)
+	{
+	}
+
+
+	public function commandAction($com)
+	{
+	}
+	
+	public function commandInterpretation($com)
+	{
+	}
+
+}
--- do/marsRover.php
+++ do/marsRover.php
@@ -1,1 +1,24 @@
- 
+<?php
+require_once( "../class/class.MR.php");
+
+$validDirections = 'NSEW';
+$northDirection = 'N';
+$southDirection = 'S';
+$eastDirection = 'E';
+$westDirection = 'W';
+$validCommands = 'LRM';
+$leftCommand = 'L';
+$rightCommand = 'R';
+$moveCommand = 'M';
+$format_error = false;
+
+
+		$MR = new MR();
+	
+
+	$MR->commandInterpretation($_POST['command']);
+
+
+include('../view/MR.html');
+
+?>
